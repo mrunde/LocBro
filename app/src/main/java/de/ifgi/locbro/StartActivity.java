7b8966b702928847ff0fb794b09556c91af563c1
cheetah90 <cheetah0513@gmail.com>
2014-10-24 10:59:28 -0500

    Add the OnItemClick to items in the list

--- app/src/main/java/de/ifgi/locbro/StartActivity.java
+++ app/src/main/java/de/ifgi/locbro/StartActivity.java
@@ -1,6 +1,7 @@
 package de.ifgi.locbro;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,7 +17,7 @@ public class StartActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        String values[]= new String[] {"Google Map", "Navigation", "Game", "Monitor Your Children"};
+        String values[]= new String[] {"Map", "Navigation", "Game", "MonitorYourChildren"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, values);
@@ -45,7 +46,28 @@ public class StartActivity extends ListActivity {
 
     @Override
     protected void onListItemClick(ListView l, View w, int position, long id){
+        /* Spot Check test
         String item = (String) getListAdapter().getItem(position);
         Toast.makeText(this, item+ " selected", Toast.LENGTH_LONG).show();
+        */
+
+        //TODO: Create the activityClass for different apps.
+        switch ( position){
+            case 0:  Intent newActivity = new Intent(this, map.class);
+                startActivity(newActivity);
+                break;
+            case 1:  Intent newActivity = new Intent(this, navigation.class);
+                startActivity(newActivity);
+                break;
+            case 2:  Intent newActivity = new Intent(this, game.class);
+                startActivity(newActivity);
+                break;
+            case 3:  Intent newActivity = new Intent(this, monitoryourchild.class);
+                startActivity(newActivity);
+                break;
+            case 4:  Intent newActivity = new Intent(this, somethingelse.class);
+                startActivity(newActivity);
+                break;
+        }
     }
 }
