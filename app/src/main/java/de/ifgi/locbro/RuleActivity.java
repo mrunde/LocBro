a030580b1f429dce00ae1f64d5ff22b54379f382
Marius Runde <marius.runde@uni-muenster.de>
2014-10-30 15:28:07 +0100

    Update RulesActivity and RuleActivity

--- app/src/main/java/de/ifgi/locbro/RuleActivity.java
+++ app/src/main/java/de/ifgi/locbro/RuleActivity.java
@@ -1,20 +1,42 @@
 package de.ifgi.locbro;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
 
 /**
  * @author Marius Runde
  */
-public class RuleActivity extends Activity {
+public class RuleActivity extends Activity implements View.OnClickListener {
 
     /**
      * Create new rule or edit existing one
      */
     private boolean isNew;
+
+    // Time and geo fence information
+    private int start_hour;
+    private int start_minute;
+    private int end_hour;
+    private int end_minute;
+    private double lat;
+    private double lng;
+
+    // GUI elements
+    private Spinner spinner_accuracy;
+    private ToggleButton tb_area;
+    private ToggleButton tb_time;
+    private Button btn_cancel;
+    private Button btn_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +46,33 @@ public class RuleActivity extends Activity {
         // Get Intent information
         Intent intent = getIntent();
         this.isNew = intent.getBooleanExtra("new_rule", true);
+
+        setupGUI();
     }
 
+    /**
+     * Setup the GUI
+     */
+    private void setupGUI() {
+        // 'Accuracy' spinner
+        this.spinner_accuracy = (Spinner) findViewById(R.id.spinner_accuracy);
+
+        // 'Area' toggle button
+        this.tb_area = (ToggleButton) findViewById(R.id.tb_area);
+        tb_area.setOnClickListener(this);
+
+        // 'Time' toggle button
+        this.tb_time = (ToggleButton) findViewById(R.id.tb_time);
+        tb_time.setOnClickListener(this);
+
+        // 'Cancel' button
+        this.btn_cancel = (Button) findViewById(R.id.btn_cancel);
+        btn_cancel.setOnClickListener(this);
+
+        // 'Save' button
+        this.btn_save = (Button) findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,5 +91,47 @@ public class RuleActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent resultIntent = new Intent(this, SettingsActivity.class);
+        setResult(Activity.RESULT_CANCELED, resultIntent);
+        finish();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.equals(findViewById(R.id.tb_area))) {
+            if (this.tb_area.isChecked()) {
+                // TODO store area
+            } else {
+                this.start_hour = -1;
+                this.start_minute = -1;
+                this.end_hour = -1;
+                this.end_minute = -1;
+            }
+        } else if (view.equals(findViewById(R.id.tb_time))) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR);
+            int minute = c.get(Calendar.MINUTE);
+            new TimePickerDialog(this, null, hour, minute, true);
+        } else if (view.equals(findViewById(R.id.btn_cancel))) {
+            Intent resultIntent = new Intent(this, SettingsActivity.class);
+            setResult(Activity.RESULT_CANCELED, resultIntent);
+            finish();
+        } else if (view.equals(findViewById(R.id.btn_save))) {
+            Intent resultIntent = new Intent(this, SettingsActivity.class);
+            resultIntent.putExtra("new_rule", this.isNew);
+            resultIntent.putExtra("start_hour", this.start_hour);
+            resultIntent.putExtra("start_minute", this.start_minute);
+            resultIntent.putExtra("end_hour", this.end_hour);
+            resultIntent.putExtra("end_minute", this.end_minute);
+            resultIntent.putExtra("lat", this.lat);
+            resultIntent.putExtra("lng", this.lng);
+            setResult(Activity.RESULT_OK, resultIntent);
+            finish();
+        }
     }
 }
--- app/src/main/java/de/ifgi/locbro/RulesActivity.java
+++ app/src/main/java/de/ifgi/locbro/RulesActivity.java
@@ -9,12 +9,22 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Activity to manage the rules of the selected application
  *
  * @author Marius Runde
  */
 public class RulesActivity extends ListActivity {
+
+    public final static int NEW_RULE_REQUEST = 1;
+
+    /**
+     * List of rules
+     */
+    private List<Rule> rules = new ArrayList<Rule>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +34,7 @@ public class RulesActivity extends ListActivity {
         Intent intent = getIntent();
         setTitle(getResources().getString(R.string.title_activity_rules) + " " + intent.getStringExtra("app_name"));
 
-        String values[] = new String[]{"Map", "Navigation", "Game", "MonitorYourChildren"};
+        String values[] = new String[]{"Map", "Navigation", "Game", "MonitorYourChildren"}; // TODO
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, values);
@@ -46,10 +56,12 @@ public class RulesActivity extends ListActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.menu_add_rule) {
-            return true;
-        } else if (id == R.id.menu_delete_rule) {
+            Intent ruleActivity = new Intent(this, RuleActivity.class);
+            ruleActivity.putExtra("new_rule", true);
+            startActivityForResult(ruleActivity, NEW_RULE_REQUEST);
             return true;
         } else if (id == R.id.menu_delete_all_rules) {
+            rules.clear();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -57,30 +69,26 @@ public class RulesActivity extends ListActivity {
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-        // Start the SettingsActivity for the selected application
-        Intent settingsActivity = new Intent(this, SettingsActivity.class);
-        settingsActivity.putExtra("selected_accuracy", 1); // TODO
-        switch (position) {
-            case 0:
-                settingsActivity.putExtra("app_name", this.getListAdapter().getItem(0).toString());
-                startActivity(settingsActivity);
-                break;
-            case 1:
-                settingsActivity.putExtra("app_name", this.getListAdapter().getItem(1).toString());
-                startActivity(settingsActivity);
-                break;
-            case 2:
-                settingsActivity.putExtra("app_name", this.getListAdapter().getItem(2).toString());
-                startActivity(settingsActivity);
-                break;
-            case 3:
-                settingsActivity.putExtra("app_name", this.getListAdapter().getItem(3).toString());
-                startActivity(settingsActivity);
-                break;
-            case 4:
-                settingsActivity.putExtra("app_name", this.getListAdapter().getItem(4).toString());
-                startActivity(settingsActivity);
-                break;
+        // TODO
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == this.NEW_RULE_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // Get the result
+                int start_hour = data.getIntExtra("start_hour", 0);
+                int start_minute = data.getIntExtra("start_minute", 0);
+                int end_hour = data.getIntExtra("end_hour", 0);
+                int end_minute = data.getIntExtra("end_minute", 0);
+                double lat = data.getDoubleExtra("lat", 0);
+                double lng = data.getDoubleExtra("lng", 0);
+
+                // Create the new rule
+                rules.add(new Rule(start_hour, start_minute, end_hour, end_minute, lat, lng));
+            }
         }
     }
 }
